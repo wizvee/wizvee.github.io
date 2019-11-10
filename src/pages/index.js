c3b0bb0fb4ed0f0d277c24c6cbabfb79f4c39ece
wizvee <jeonghye.dev@gmail.com>
2019-11-11 01:35:38 +0900

    posting 진입까지 적용

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { Link, graphql } from 'gatsby'
 import styled from 'styled-components'
 import Layout from '../components/Layout'
 import Profile from '../components/Profile'
 
-const Article = styled.article`
+const Article = styled(Link)`
   header {
     margin-bottom: 0.5rem;
     h3 {
       margin-bottom: 0.3rem;
     }
+  }
+  p {
+    font-size: 0.9rem;
   }
 `
 
@@ -18,7 +21,7 @@ const Index = ({ data }) => {
     <Layout>
       <Profile />
       {data.allMarkdownRemark.edges.map(({ node }) => (
-        <Article key={node.id}>
+        <Article key={node.id} to={node.fields.slug}>
           <header>
             <h3 className="primary">{node.frontmatter.title}</h3>
             <small>{node.frontmatter.date}</small>
@@ -37,12 +40,14 @@ export const query = graphql`
     allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
       edges {
         node {
-          id
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
           }
-          excerpt(pruneLength: 200)
+          fields {
+            slug
+          }
+          excerpt(truncate: true)
         }
       }
     }
