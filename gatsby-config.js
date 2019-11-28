0df3d13227df9e92f542c9c2d58cbf4fcf1b29bf
wizvee <jeonghye.dev@gmail.com>
2019-11-28 14:38:58 +0900

    react-icons 이슈 수정 중

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,7 +69,7 @@ module.exports = {
       resolve: 'gatsby-plugin-react-svg',
       options: {
         rule: {
-          include: /assets/,
+          include: 'react-icons/',
         },
       },
     },
