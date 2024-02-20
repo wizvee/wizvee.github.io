1735318ace9e47349d9c0f117d0ce00dcd8a08b1
wizvee <jeonghye.dev@gmail.com>
2024-02-21 04:24:52 +0900

    🔨 Refactor: Update async file handling in lib/api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,33 +1,43 @@
-import fs from "fs";
 import { join } from "path";
+import fs from "fs/promises";
 import { compileMDX } from "next-mdx-remote/rsc";
 
 type Frontmatter = {
-  date: string;
+  title: string;
+  date: Date;
 };
 
 export type Post = {
   slug: string;
   content: any;
-  frontmatter: Frontmatter;
+  title: string;
+  date: string;
 };
 
 const postsDirectory = join(process.cwd(), "_posts");
+
+function dateTimeFormat(date: Date): string {
+  return new Intl.DateTimeFormat("ko-KR").format(date);
+}
 
 export async function getPostBySlug(slug: string) {
   const realSlug = slug.replace(/\.mdx$/, "");
   const fullPath = join(postsDirectory, `${realSlug}.mdx`);
 
-  const { content, frontmatter } = await compileMDX<Frontmatter>({
-    source: fs.readFileSync(fullPath, "utf8"),
+  const fileContents = await fs.readFile(fullPath, "utf8");
+  const {
+    content,
+    frontmatter: { title, date },
+  } = await compileMDX<Frontmatter>({
+    source: fileContents,
     options: { parseFrontmatter: true },
   });
 
-  return { slug: realSlug, content, frontmatter } as Post;
+  return { slug: realSlug, content, title, date: dateTimeFormat(date) } as Post;
 }
 
 export async function getAllPosts(): Promise<Post[]> {
-  const slugs = fs.readdirSync(postsDirectory);
-  const posts = await Promise.all(slugs.map((slug) => getPostBySlug(slug)));
+  const slugs = await fs.readdir(postsDirectory);
+  const posts = await Promise.all(slugs.map(getPostBySlug));
   return posts;
 }
