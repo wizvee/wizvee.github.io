0544bbf964dd0161af318811484da34395048b2c
wizvee <jeonghye.dev@gmail.com>
2024-02-24 18:58:45 +0900

    ♻️ Refactor types and extract frontmatter processing

--- lib/api.ts
+++ lib/api.ts
@@ -1,18 +1,22 @@
 import { join } from "path";
 import fs from "fs/promises";
+import { ReactElement } from "react";
 import { compileMDX } from "next-mdx-remote/rsc";
 
 type Frontmatter = {
   title: string;
   date: Date;
+  tags: string[];
+};
+
+type FrontmatterProcessed = Omit<Frontmatter, "date"> & {
+  date: string;
 };
 
 export type Post = {
   slug: string;
-  content: any;
-  title: string;
-  date: string;
-};
+  content: ReactElement;
+} & FrontmatterProcessed;
 
 const postsDirectory = join(process.cwd(), "_posts");
 
@@ -20,20 +24,26 @@ function dateTimeFormat(date: Date): string {
   return new Intl.DateTimeFormat("ko-KR").format(date);
 }
 
+function processFrontmatter(data: Frontmatter): FrontmatterProcessed {
+  const { title, date, tags } = data;
+  return { title, date: dateTimeFormat(date), tags };
+}
+
 export async function getPostBySlug(slug: string) {
   const realSlug = slug.replace(/\.mdx$/, "");
   const fullPath = join(postsDirectory, `${realSlug}.mdx`);
 
   const fileContents = await fs.readFile(fullPath, "utf8");
-  const {
-    content,
-    frontmatter: { title, date },
-  } = await compileMDX<Frontmatter>({
+  const { content, frontmatter } = await compileMDX<Frontmatter>({
     source: fileContents,
     options: { parseFrontmatter: true },
   });
 
-  return { slug: realSlug, content, title, date: dateTimeFormat(date) } as Post;
+  return {
+    slug: realSlug,
+    content,
+    ...processFrontmatter(frontmatter),
+  } as Post;
 }
 
 export async function getAllPosts(): Promise<Post[]> {
